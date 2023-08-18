01b593ab2c3407f21399bd8b3f86e68cc83679f6
balachandregowda <balagowda9483@gmail.com>
2023-08-18 07:10:42 +0530

    error page and contact page correction

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,14 +1,15 @@
 import './Error.css';
+import imeg from './pageError.jpg';
 
 function ErrorPage() {
   return (
     <>
       <div className="error">
         <div className="error-image">
-          <img src="error.jpg" alt="loading" />
+          <img src={imeg} alt="loading" />
         </div>
         <div className="error-text">
-          <h1 className="error-heading">Sorry I am Unable to find this Page.</h1>
+          {/* <h1 className="error-heading">Sorry I am Unable to find this Page.</h1> */}
         </div>
       </div>
     </>
